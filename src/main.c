65e37a18578fff0275f1e0828cfbe304bfcca7a3
microbuilder <contact@microbuilder.eu>
2013-02-19 22:20:38 +0100

    Cleaned up for release

--- src/main.c
+++ src/main.c
@@ -1,12 +1,41 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+/**************************************************************************/
+/*!
+    @file     main.c
+    @author   K. Townsend (microBuilder.eu)
 
+    @section LICENSE
+
+    Software License Agreement (BSD License)
+
+    Copyright (c) 2013, K. Townsend
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holders nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/**************************************************************************/
 #include "projectconfig.h"
 #include "core/systick/systick.h"
 #include "boards/board.h"
-#include "messages.h"
 
 #ifdef CFG_INTERFACE
   #include "cli/cli.h"
@@ -21,100 +50,11 @@
   #endif
 #endif
 
-#ifdef CFG_CHIBI
-  #include "drivers/rf/chibi/chb.h"
-  #include "drivers/rf/chibi/chb_drvr.h"
-  static chb_rx_data_t rx_data;
-#endif
-
 #if defined(__CODE_RED)
   #include <cr_section_macros.h>
   #include <NXP/crp.h>
   __CRP const unsigned int CRP_WORD = CRP_NO_CRP ;
 #endif
-
-#include "drivers/sensors/accelerometers/lsm303accel.h"
-#include "drivers/sensors/magnetometers/lsm303mag.h"
-#include "drivers/sensors/pressure/bmp085.h"
-
-#ifdef CFG_CHIBI
-/**************************************************************************/
-/*!
-    Converts the ED (Energy Detection) value to dBm using the following
-    formula: dBm = RSSI_BASE_VAL + 1.03 * ED
-
-    For more information see section 6.5 of the AT86RF212 datasheet
-*/
-/**************************************************************************/
-int edToDBM(uint32_t ed)
-{
-  #if CFG_CHIBI_MODE == 0 || CFG_CHIBI_MODE == 1 || CFG_CHIBI_MODE == 2
-    // Calculate for OQPSK (RSSI Base Value = -100)
-    int dbm = (103 * ed - 10000);
-  #else
-    // Calculate for BPSK (RSSI Base Value = -98)
-    int dbm = (103 * ed - 9800);
-  #endif
-
-  return dbm / 100;
-}
-
-void sendMessage(void)
-{
-  uint8_t msgbuf[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-  if(msgSend(0xFFFF, MSG_MESSAGETYPE_NONE, msgbuf, 10))
-  {
-    printf("Message TX failure%s", CFG_PRINTF_NEWLINE);
-  }
-}
-
-void checkForMessages(void)
-{
-  chb_pcb_t *pcb = chb_get_pcb();
-
-  while (pcb->data_rcv)
-  {
-    // Enable LED to indicate message reception
-    boardLED(CFG_LED_ON);
-    // get the length of the data
-    rx_data.len = chb_read(&rx_data);
-    // make sure the length is nonzero
-    if (rx_data.len)
-    {
-      int dbm = edToDBM(pcb->ed);
-      // printf("Message received from node %02X: %s, len=%d, dBm=%d.%s", rx_data.src_addr, rx_data.data, rx_data.len, dbm, CFG_PRINTF_NEWLINE);
-      printf("Message received from node 0x%04X (len=%d, dBm=%d):%s", rx_data.src_addr, rx_data.len, dbm, CFG_PRINTF_NEWLINE);
-      printf("  Message ID:   0x%04X%s", *(uint16_t*)&rx_data.data[0], CFG_PRINTF_NEWLINE);
-      printf("  Message Type: 0x%02X%s", *(uint8_t*)&rx_data.data[2], CFG_PRINTF_NEWLINE);
-      printf("  Timestamp:    %d%s", *(uint32_t*)&rx_data.data[3], CFG_PRINTF_NEWLINE);
-      printf("  Payload:      %d bytes%s", *(uint8_t*)&rx_data.data[8], CFG_PRINTF_NEWLINE);
-      if (rx_data.data[8])
-      {
-        uint8_t i;
-        printf("%s", CFG_PRINTF_NEWLINE);
-        for (i = 0; i < rx_data.data[8]; i++)
-        {
-          printf("0x%02X ", *(uint8_t*)&rx_data.data[9+i]);
-        }
-      }
-      printf("%s", CFG_PRINTF_NEWLINE);
-    }
-    // Disable LED
-    boardLED(CFG_LED_OFF);
-  }
-}
-#endif
-
-void blinkyError(void)
-{
-  while(1)
-  {
-    boardLED(CFG_LED_ON);
-    systickDelay(100);
-    boardLED(CFG_LED_OFF);
-    systickDelay(100);
-  }
-}
 
 int main(void)
 {
@@ -130,6 +70,9 @@ int main(void)
     if (currentSecond != lastSecond)
     {
       lastSecond = currentSecond;
+
+      /* Toggle LED once per second */
+      boardLED(lastSecond % 2);
 
       /* Display any incoming HID data if HID GENERIC is enabled */
       #ifdef CFG_USB_HID_GENERIC
@@ -147,26 +90,11 @@ int main(void)
           }
         }
       #endif
-
-      /* Send a message over the air */
-      #ifdef CFG_CHIBI
-        sendMessage();
-      #endif
-
-      /* Do blinky if we're not sending data wirelessly */
-      #if !defined(CFG_CHIBI)
-        boardLED(lastSecond % 2);
-      #endif
     }
 
     /* Poll for CLI input if CFG_INTERFACE is enabled */
     #ifdef CFG_INTERFACE
       cliPoll();
-    #endif
-
-    /* Check for incoming wireless messages */
-    #ifdef CFG_CHIBI
-      checkForMessages();
     #endif
   }
 
