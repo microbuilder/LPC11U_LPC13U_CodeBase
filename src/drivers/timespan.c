32ecb34d5c3399b6728942dd0ecc7c059f94b7a6
microBuilder.eu <contact@microbuilder.eu>
2013-05-04 23:06:20 +0200

    Fixed negative value bug in timespanCreate

--- src/drivers/timespan.c
+++ src/drivers/timespan.c
@@ -60,13 +60,13 @@ error_t timespanCreate(int64_t ticks, timespan_t *timespan)
 
   memcpy(timespan, 0, sizeof(timespan_t));
 
-  timespan->days = (ticks / TIMESPAN_NANOSPERDAY) * (ticks < 0 ? -1 : 1);
-  timespan->hours = ((ticks / TIMESPAN_NANOSPERHOUR) % 24) * (ticks < 0 ? -1 : 1);
-  timespan->minutes = ((ticks / TIMESPAN_NANOSPERMINUTE) % 60) * (ticks < 0 ? -1 : 1);
-  timespan->seconds = ((ticks / TIMESPAN_NANOSPERSECOND) % 60) * (ticks < 0 ? -1 : 1);
-  timespan->milliseconds = ((ticks / TIMESPAN_NANOSPERMILLISECOND) % 1000) * (ticks < 0 ? -1 : 1);
-  timespan->microseconds = ((ticks / TIMESPAN_NANOSPERMICROSECOND) % 1000) * (ticks < 0 ? -1 : 1);
-  timespan->nanoseconds = (ticks % 1000) * (ticks < 0 ? -1 : 1);
+  timespan->days = (ticks / TIMESPAN_NANOSPERDAY);
+  timespan->hours = (ticks / TIMESPAN_NANOSPERHOUR) % 24;
+  timespan->minutes = (ticks / TIMESPAN_NANOSPERMINUTE) % 60;
+  timespan->seconds = (ticks / TIMESPAN_NANOSPERSECOND) % 60;
+  timespan->milliseconds = (ticks / TIMESPAN_NANOSPERMILLISECOND) % 1000;
+  timespan->microseconds = (ticks / TIMESPAN_NANOSPERMICROSECOND) % 1000;
+  timespan->nanoseconds = (ticks % 1000);
   timespan->__ticks = ticks;
 
   return ERROR_NONE;
