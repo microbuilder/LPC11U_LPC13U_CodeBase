4da9c7fd3abcf09bceffd0a79744d4c9c8a57885
microBuilder.eu <contact@microbuilder.eu>
2013-06-11 07:28:38 +0200

    Updated to use TIMER16[0] or systick

--- src/core/delay/delay.c
+++ src/core/delay/delay.c
@@ -35,6 +35,9 @@
 /**************************************************************************/
 #include "delay.h"
 
+/* Set this to 1 to use TIMER16[0] instead of systick */
+#define DELAY_USE_TIMER16_0      (0)
+
 #ifdef CFG_SDCARD
 #include "drivers/storage/fatfs/diskio.h"
 volatile uint32_t fatTicks = 0;
@@ -43,6 +46,7 @@ volatile uint32_t fatTicks = 0;
 volatile uint32_t delayTicks = 0;
 volatile uint32_t delayRollovers = 0;
 
+#if DELAY_USE_TIMER16_0
 /**************************************************************************/
 /*!
     @brief Interrupt handler for 16-bit timer 0
@@ -72,6 +76,29 @@ void CT16B0_IRQHandler(void)
 
   return;
 }
+#else
+/**************************************************************************/
+/*!
+    @brief Systick interrupt handler
+*/
+/**************************************************************************/
+void SysTick_Handler (void)
+{
+  delayTicks++;
+  if (delayTicks == 0) delayRollovers++;
+
+  #ifdef CFG_SDCARD
+  fatTicks++;
+  if (fatTicks == 10)
+  {
+    fatTicks = 0;
+    disk_timerproc();
+  }
+  #endif
+
+  return;
+}
+#endif
 
 /**************************************************************************/
 /*!
@@ -83,24 +110,42 @@ void delayInit (void)
   delayTicks = 0;
   delayRollovers = 0;
 
-  /* Power up the timer */
-  LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 7);
+  #if DELAY_USE_TIMER16_0
+    /* Power up the timer */
+    LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 7);
 
-  LPC_CT16B0->TCR  = 0x02;            /* Reset the timer */
-  LPC_CT16B0->PR   = 0x03;            /* Set prescaler to four */
-  LPC_CT16B0->IR   = 0xff;            /* Reset all interrrupts */
-  LPC_CT16B0->PWMC = 0x00;            /* Disable PWM mode */
-  LPC_CT16B0->MR0  = (SystemCoreClock / 1000) >> 2; /* 1 ms w/prescalar */
-  LPC_CT16B0->MCR  = (0x3<<0);        /* Interrupt and Reset on MR0 */
+    LPC_CT16B0->TCR  = 0x02;            /* Reset the timer */
+    LPC_CT16B0->PR   = 0x03;            /* Set prescaler to four */
+    LPC_CT16B0->IR   = 0xff;            /* Reset all interrrupts */
+    LPC_CT16B0->PWMC = 0x00;            /* Disable PWM mode */
+    LPC_CT16B0->MR0  = (SystemCoreClock / 1000) >> 2; /* 1 ms w/prescalar */
+    LPC_CT16B0->MCR  = (0x3<<0);        /* Interrupt and Reset on MR0 */
 
-  /* Enable the interrupt */
-  #if defined CFG_MCU_FAMILY_LPC11UXX
-  NVIC_EnableIRQ(TIMER_16_0_IRQn);
-  #elif defined CFG_MCU_FAMILY_LPC13UXX
-  NVIC_EnableIRQ(CT16B0_IRQn);
+    /* Enable the interrupt */
+    #if defined CFG_MCU_FAMILY_LPC11UXX
+    NVIC_EnableIRQ(TIMER_16_0_IRQn);
+    #elif defined CFG_MCU_FAMILY_LPC13UXX
+    NVIC_EnableIRQ(CT16B0_IRQn);
+    #endif
+
+    LPC_CT16B0->TCR  = 0x01;            /* Start timer */
+  #else
+    uint32_t ticks = (SystemCoreClock / 1000);
+
+    /* Reset counter */
+    delayTicks = 0;
+
+    /* Set reload register */
+    SysTick->LOAD  = (ticks & SYSTICK_STRELOAD_MASK) - 1;
+
+    /* Load the systick counter value */
+    SysTick->VAL = 0;
+
+    /* Enable systick IRQ and timer */
+    SysTick->CTRL = SYSTICK_STCTRL_CLKSOURCE |
+                    SYSTICK_STCTRL_TICKINT |
+                    SYSTICK_STCTRL_ENABLE;
   #endif
-
-  LPC_CT16B0->TCR  = 0x01;            /* Start timer */
 }
 
 /**************************************************************************/
