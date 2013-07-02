aa8372a4eb91d34c90e6b16ab3fcfa5e965652f5
microBuilder.eu <contact@microbuilder.eu>
2013-07-02 02:47:26 +0200

    travis-ci fix

--- src/boards/lpcxpresso1347/board_lpcxpresso1347.h
+++ src/boards/lpcxpresso1347/board_lpcxpresso1347.h
@@ -679,7 +679,7 @@ extern "C" {
 
       // #define CFG_USB_HID_KEYBOARD
       // #define CFG_USB_HID_MOUSE
-      // #define CFG_USB_HID_GENERIC
+      #define CFG_USB_HID_GENERIC
       #define CFG_USB_HID_GENERIC_REPORT_SIZE (64)
 
       // #define CFG_USB_MSC
