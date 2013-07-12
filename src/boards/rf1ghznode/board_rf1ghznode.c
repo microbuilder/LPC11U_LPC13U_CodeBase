f356df2c5fb9b05faf3d82ce6d0338fc8212cf2c
microBuilder.eu <contact@microbuilder.eu>
2013-07-12 12:23:43 +0200

    Updated header locations

--- src/boards/rf1ghznode/board_rf1ghznode.c
+++ src/boards/rf1ghznode/board_rf1ghznode.c
@@ -50,9 +50,9 @@
 #include "core/adc/adc.h"
 
 #ifdef CFG_CHIBI
-  #include "drivers/rf/chibi/chb.h"
-  #include "drivers/rf/chibi/chb_drvr.h"
-  #include "drivers/rf/chibi/messages.h"
+  #include "drivers/rf/802.15.4/chibi/chb.h"
+  #include "drivers/rf/802.15.4/chibi/chb_drvr.h"
+  #include "drivers/rf/802.15.4/chibi/messages.h"
   static chb_rx_data_t rx_data;
 #endif
 
