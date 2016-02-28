8aa0550ac41a601e3539c2deeddb6c7f064adfa4
Kevin Townsend <contact AT microbuilder D0T eu>
2016-02-28 13:11:30 +0100

    Added 2 pole butterworth coefficient calculator

--- src/drivers/filters/iir/iir.c
+++ src/drivers/filters/iir/iir.c
@@ -4,12 +4,15 @@
    @brief    Helper functions for IIR filters generated with Octave or
              Matlab (supports 1, 2 or 3-pole filters)
 
-   @note     Shamelessly take from Holly Gates tutorial on using Octave
+   @note     Shamelessly taken from Holly Gates tutorial on using Octave
              to calculate IIR/FIR filters:
              http://tooling-up.blogspot.fr/2013/06/signal-acquisition-filtering-on-simple.html
 */
 /**************************************************************************/
 #include "iir.h"
+#include <math.h>
+
+#define IIR_PI 3.14159265358979323846
 
 /**************************************************************************/
 /*!
@@ -54,6 +57,10 @@ float32_t iir_filt_1p(iir_filt_1p_instance* filt, float32_t in)
 /**************************************************************************/
 /*!
     @brief     Two pole IIR filter routine
+
+               For a two pole butterworth filter, use the iis_butter2
+               function in this file to calculate the butterworth filter
+               coefficients without having to make use of Octave.
 
     @param[in] filt
                Pointer to the iir_filt_2p_instance instance containing
@@ -119,6 +126,43 @@ float32_t iir_filt_2p(iir_filt_2p_instance* filt, float32_t in)
   filt->out = (filt->b0*in +
                filt->b1*filt->x1 + filt->b2*filt->x2 -
                filt->a1*filt->y1 - filt->a2*filt->y2);
+
+  /* Shift input samples */
+  filt->x2 = filt->x1;
+  filt->x1 = in;
+
+  /* Shift output samples */
+  filt->y2 = filt->y1;
+  filt->y1 = filt->out;
+
+  return(filt->out);
+}
+
+/**************************************************************************/
+/*!
+    @brief     Two pole integer based IIR filter routine
+
+    @param[in] filt
+               Pointer to the iir_filt_i_2p_instance instance containing
+               the filter settings
+    @param[in] in
+               The int32_t value to feed into the filter
+    @returns   The int32_t filter output
+
+    @code
+
+    ToDo
+
+    @endcode
+*/
+/**************************************************************************/
+int32_t iir_filt_i_2p(iir_filt_i_2p_instance* filt, int32_t in)
+{
+  /* Calculate new output */
+  filt->out = (filt->b0*in +
+	  filt->b1*filt->x1 + filt->b2*filt->x2)>>(filt->bf);
+
+  filt->out -= ((filt->a1*filt->y1 + filt->a2*filt->y2)>>(filt->af));
 
   /* Shift input samples */
   filt->x2 = filt->x1;
@@ -220,4 +264,75 @@ float32_t iir_filt_4p(iir_filt_4p_instance* filt, float32_t in)
   filt->y1 = filt->out;
 
   return(filt->out);
+}
+
+/**************************************************************************/
+/*!
+    @brief      Two pole butterworth low pass IIR filter coefficient
+                generator routine
+
+    @param[in]  filt
+                Pointer to the iir_filt_2p_instance instance containing
+                the filter data structure
+    @param[in]  fs
+                sampling frequency
+    @param[in]  fc
+                cut point frequency
+
+    @code
+
+    iir_filt_2p_instance test_filt; // The filter structure itself
+    float fs = 100;                 // 100Hz sample frequency
+    float fc = 5;                   // 5Hz cut frequency
+    float input,output;
+
+    iir_butter2(&test_filt,fs,fc);  // Generate the filter coeffs
+
+    printf("filter coeffs:\r\n");
+    printf("test_filt  a1:%f  a2:%f  b0:%f  b1:%f  b2:%f\r\n",
+  	 test_filt.a1,test_filt.a2,test_filt.b0,test_filt.b1,test_filt.b2);
+
+    input = 10.0;
+
+    output = iir_filt_2p(&test_filt,input); // Run the filter once
+    // Note: You need to run the filter at the frequency you specified for Fs!
+
+    printf("input:%f  output:%f\r\n", input, output);
+
+    @endcode
+
+    You can verify the output of this filter against Octave as follows:
+
+    OUTPUT at terminal when run with fs=100 fc=5
+    -------------------------------
+    filter coeffs:
+    test_filt  a1:-1.561018  a2:0.641352  b0:0.020083  b1:0.040167  b2:0.020083
+    input:10.000000  output:0.200834
+
+    OCTAVE design: 2 pole, fc=5, fs=50 (Fnyq = 1/2 Fs)
+    -------------------------------
+    octave:4> pkg load signal
+    octave:5> [b a] = butter(2,5/50)
+    b =
+       0.020083   0.040167   0.020083
+
+    a =
+       1.00000  -1.56102   0.64135
+*/
+/**************************************************************************/
+void iir_butter2(iir_filt_2p_instance* filt, float32_t fs, float32_t fc)
+{
+  float ax  = tan(IIR_PI*fc/fs);
+  float ax2 = ax*ax;
+  float r   = sin(IIR_PI*3.0/4.0);
+  float sx  = ax2 + 2.0*ax*r + 1.0;
+  float A   = ax2/sx;
+  float d1  = 2.0*(1-ax2)/sx;
+  float d2  = -(ax2 - 2.0*ax*r + 1.0)/sx;
+
+  filt->b0 = A;
+  filt->b1 = 2*A;
+  filt->b2 = A;
+  filt->a1 = -1*d1;
+  filt->a2 = -1*d2;
 }
--- src/drivers/filters/iir/iir.h
+++ src/drivers/filters/iir/iir.h
@@ -38,6 +38,23 @@ typedef struct
   float32_t b2;
 } iir_filt_2p_instance;
 
+/* 2-pole integer IIR filter data structure */
+typedef struct
+{
+  int32_t x1;
+  int32_t x2;
+  int32_t out;
+  int32_t y1;
+  int32_t y2;
+  uint8_t af;
+  int32_t a1;
+  int32_t a2;
+  uint8_t bf;
+  int32_t b0;
+  int32_t b1;
+  int32_t b2;
+} iir_filt_i_2p_instance;
+
 /* 3-pole IIR filter data structure */
 typedef struct
 {
@@ -80,10 +97,13 @@ typedef struct
   float32_t b4;
 } iir_filt_4p_instance;
 
-float32_t iir_filt_1p(iir_filt_1p_instance* filt, float32_t in);
-float32_t iir_filt_2p(iir_filt_2p_instance* filt, float32_t in);
-float32_t iir_filt_3p(iir_filt_3p_instance* filt, float32_t in);
-float32_t iir_filt_4p(iir_filt_4p_instance* filt, float32_t in);
+float32_t iir_filt_1p   ( iir_filt_1p_instance* filt, float32_t in );
+float32_t iir_filt_2p   ( iir_filt_2p_instance* filt, float32_t in );
+int32_t   iir_filt_i_2p ( iir_filt_i_2p_instance* filt, int32_t in );
+float32_t iir_filt_3p   ( iir_filt_3p_instance* filt, float32_t in );
+float32_t iir_filt_4p   ( iir_filt_4p_instance* filt, float32_t in );
+
+void      iir_butter2 ( iir_filt_2p_instance* filt, float32_t fs, float32_t fc );
 
 #ifdef __cplusplus
 }
